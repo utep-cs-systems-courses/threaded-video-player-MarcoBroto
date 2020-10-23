043cfa2f35d21a65cca59b350c5758b4e8b5383d
MarcoBroto <msoto9940@gmail.com>
2020-10-22 20:18:34 -0600

    Solved memory leak with flag

--- video2grayscale.py
+++ video2grayscale.py
@@ -1,85 +1,84 @@
 #! python3.8
 
 from threading import Thread, Semaphore
-from queue import Queue
-import cv2 as cv
-from cv_functions import *
+import sys, cv2 as cv
 
-class BlockingQueue():
-	def __init__(self, max_size=10, name=''):
-		self._queue_sema = Semaphore(max_size)
-		self._dequeue_sema = Semaphore(0)
-		self.name = name
+
+class BlockingQueue(): # Blocking Queue implemented with list; less ops/sec
+	def __init__(self, max_size=10):
+		self._queue_sema = Semaphore(max_size) # Tracks num of available incoming items
+		self._dequeue_sema = Semaphore(0) # Tracks num of available outgoing items
 		self._items = []
 
 	def get(self):
-		# print(f'\t{self.name} dequeu acquire')
-		self._dequeue_sema.acquire()
+		self._dequeue_sema.acquire() # Remove spot for outgoing items
 		if len(self._items):
 			val = self._items.pop(0)
-			# print(f'\t{self.name} queue release')
-			self._queue_sema.release()
+			self._queue_sema.release() # Add spot for incoming items
 			return val
-		else:
-			return None
+		else: return None
 
 	def put(self, value):
-		# print(f'\t{self.name} queue acquire')
-		self._queue_sema.acquire()
+		self._queue_sema.acquire() # Remove spot from incoming items
 		self._items.append(value)
-		# print(f'\t{self.name} dequeue release')
-		self._dequeue_sema.release()
+		self._dequeue_sema.release() # Add spot for outgoing items
 
 	def getSize(self): return len(self._items)
 
-default_file = 'clip.mp4'
-default_delay = 42
-QUEUE_SIZE = 10
 
-extractQueue, displayQueue = BlockingQueue(QUEUE_SIZE, "extract"), BlockingQueue(QUEUE_SIZE, "display")
+default_file = 'clip.mp4' # Path to video file
+frame_delay = 1 # Delay after rendering frames
+QUEUE_SIZE = 10 # Number of elements a queue can hold at one time
+
+extractQueue, displayQueue = BlockingQueue(QUEUE_SIZE), BlockingQueue(QUEUE_SIZE)
+complete = False # Global flag for determining if no frames left in video
 
 
 def extractFrames(filePath):
-	print('\t\t--Started Extract Thread--')
-	vid = openVideoFile(filePath)
-	success, frame = nextFrame(vid)
+	print('--Starting Extract Thread--')
+	video = cv.VideoCapture(filePath) # Open video
+	success, frame = video.read() # Get first video frame
 	count = 1
 	while success:
 		try:
-			extractQueue.put(frame)
-			print(f'extracting frame {count}')
-			success, frame = nextFrame(vid)
-		except Exception as err: print(str(err) + '\tfail 1')
+			extractQueue.put(frame) # Add frame to extract queue to be converted
+			print(f'\textracting frame {count}\n')
+			success, frame = video.read() # Get next video frame
+		except Exception as err: print(err)
 		finally: count += 1
+	global complete
+	complete = True
 
 
 def convertFramesToGrayscale():
-	print('\t\t--Started Convert Thread--')
+	print('--Starting Convert Thread--')
 	count = 1
 	while True:
 		try:
-			frame = extractQueue.get()
-			print(f'converting frame {count} to grayscale')
-			frame = frame2grayscale(frame)
-			displayQueue.put(frame)
-			# displayQueue.put(frame2grayscale(inputQueue.get()))
-		except Exception as err: print(str(err) + '\tfail 2')
+			frame = cv.cvtColor(extractQueue.get(), cv.COLOR_BGR2GRAY) # Convert frame to grayscale
+			print(f'\t\tconverting frame {count} to grayscale\n')
+			displayQueue.put(frame) # Add frame to display queue
+		except Exception as err: print(err)
 		finally: count += 1
 
 
 def displayFrames():
-	print('\t\t--Started Display Thread--')
+	print('--Starting Display Thread--')
 	count = 1
-	while True:
+	while displayQueue.getSize() > 0 or not complete:
 		try:
-			displayFrame(displayQueue.get(), default_delay)
-			print(f'displaying frame {count}')
-		except Exception as err: print(str(err) + '\tfail 3')
+			frame = displayQueue.get()
+			cv.imshow('Video', frame)  # Display the frame in a window called "Video"
+			cv.waitKey(frame_delay)  # Delay execution
+			print(f'\t\t\tdisplaying frame {count}\n')
+		except Exception as err: print(err)
 		finally: count += 1
-	cv.destroyAllWindows()
+	cv.destroyAllWindows() # Close video window
 
 
 if __name__ == '__main__':
-	Thread(target=extractFrames, args=[default_file], name='Extract Frames Thread').start()
-	Thread(target=convertFramesToGrayscale, name='Convert To Grayscale Thread', daemon=True).start()
-	displayFrames()
+	try:
+		Thread(target=extractFrames, args=[default_file], name='Extract Frames Thread', daemon=True).start() # Helper thread
+		Thread(target=convertFramesToGrayscale, name='Convert To Grayscale Thread', daemon=True).start() # Helper thread
+		displayFrames() # Main thread
+	except KeyboardInterrupt: sys.exit(1)
